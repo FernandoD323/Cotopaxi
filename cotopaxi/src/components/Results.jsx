5a832034c53c17f015c8fe524c7da3372663dfba
Fernando Diaz <diazemanuel323@gmail.com>
2022-10-12 16:28:50 -0600

    Cards productos

--- cotopaxi/src/components/Results.jsx
+++ cotopaxi/src/components/Results.jsx
@@ -19,18 +19,55 @@ export const Results = () => {
 
             <div className='option1'>
                 <div className='option1__frame36'>
-                    <div className='option1__frame36__img'>
+                    <div className=''>
                         <div className='option1__frame36__img1' >
-                            <div className='option1__frame36__img' >
+                            <div className='option1__frame36__match'>
+                                <div className='match__great'>
+                                    the Best match
+                                </div>
+                                <div className='frame37'>
+                                    <div className='frame37__match'>100% match</div>
+                                    <div className='frame37__button'>
+                                        <div className='frame37__button__elipse'>
+                                            <div className='frame37__button__group'>
+                                                <div className='line1'></div>
+                                                <div className='line2'></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <div className='option1__frame36__img' >
+                                    <div className='button'>
+                                        <div className="button__text">Choose Color</div>
+                                    </div>
+                                </div>
 
                             </div>
                         </div>
                     </div>
-
                 </div>
 
+                <div className='option1__content'>
+                    <div className="top">
+                        <div className='top__text1'>Luzon 18L Backpack - Del Día</div>
+                        <div className='top__text2'>$60</div>
+                    </div>
+                    <div className="text3">Great for Hikes</div>
+                </div>
+            </div>
 
-                <div className='option1__content'></div>
+            <div className="frame41">
+                <div className="frame41__img"></div>
+                <div className="frame41__frame40">
+                    <div className='frame41__frame40__text1'>Vibrant, one-of-a-kind colorways designed
+                        from 100% repurposed fabrics. Responsibly made, uniquely yours.</div>
+                        
+                    <div className='frame41__frame40__text2'>Del Día bags are made in the Philippines
+                        using fabric left over from other companies’ production runs. This helps keep good
+                        material out of the landfill, and puts it into the hands of thoughtful adventurers like you.</div>
+                </div>
             </div>
         </div>
     )

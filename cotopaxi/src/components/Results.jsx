6af320e10f7dba15d6b3d781b8b4e9fc4c3d11cf
Fernando Diaz <diazemanuel323@gmail.com>
2022-10-13 09:04:07 -0600

    card información

--- cotopaxi/src/components/Results.jsx
+++ cotopaxi/src/components/Results.jsx
@@ -37,24 +37,62 @@ export const Results = () => {
                                     </div>
                                 </div>
                             </div>
-                            <div>
-                                <div className='option1__frame36__img' >
-                                    <div className='button'>
-                                        <div className="button__text">Choose Color</div>
-                                    </div>
-                                </div>
 
+                            <div className='option1__frame36__img' >
+                            </div>
+                            <div className='button'>
+                                <div className="button__text">Choose Color</div>
                             </div>
                         </div>
                     </div>
                 </div>
+
 
                 <div className='option1__content'>
                     <div className="top">
                         <div className='top__text1'>Luzon 18L Backpack - Del Día</div>
                         <div className='top__text2'>$60</div>
                     </div>
-                    <div className="text3">Great for Hikes</div>
+                    <div className="text3">Great for <span className='hikes'>Hikes</span></div>
+                </div>
+            </div>
+            <div className='option1'>
+                <div className='option1__frame36'>
+                    <div className=''>
+                        <div className='option1__frame36__img1' >
+                            <div className='option1__frame36__match'>
+                                <div className='match__great'>
+                                    another great option for you
+                                </div>
+                                <div className='frame37'>
+                                    <div className='frame37__match'>80% match</div>
+                                    <div className='frame37__button'>
+                                        <div className='frame37__button__elipse'>
+                                            <div className='frame37__button__group'>
+                                                <div className='line1'></div>
+                                                <div className='line2'></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className='option1__frame36__img2' >
+                            </div>
+                            <div className='button'>
+                                <div className="button__text">Choose Color</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className='option1__content'>
+                    <div className="top">
+                        <div className='top__text1'>Luzon 18L Backpack - Del Día</div>
+                        <div className='top__text2'>$80</div>
+                    </div>
+                    <div className="text3">Great for <span className='hikes'>Hikes</span></div>
                 </div>
             </div>
 
@@ -63,10 +101,66 @@ export const Results = () => {
                 <div className="frame41__frame40">
                     <div className='frame41__frame40__text1'>Vibrant, one-of-a-kind colorways designed
                         from 100% repurposed fabrics. Responsibly made, uniquely yours.</div>
-                        
+
                     <div className='frame41__frame40__text2'>Del Día bags are made in the Philippines
                         using fabric left over from other companies’ production runs. This helps keep good
                         material out of the landfill, and puts it into the hands of thoughtful adventurers like you.</div>
+                </div>
+            </div>
+
+            <div className="frame29">
+                <div className='frame79'>
+                    <div className='frame79__img'></div>
+                </div>
+                <div className="frame28">
+                    <div className="frame38">
+                        <div className="frame38__text1">Guaranteed for good™</div>
+                        <div className="frame38__text1 size">Built To Last. Sustainably Sourced. Ethically Made.</div>
+                    </div>
+
+                    <div className='frame28__text'>Truly sustainable design includes a backup plan. If there’s a problem
+                        with your gear, we’ll happily repair it, replace it, help you return it,
+                        or reward you for trading it in.
+                    </div>
+
+                    <div className="frame28__button">
+                        <div className='frame28__button__text'>
+                            Learn More
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="frame80">
+                <div className="frame30">
+                    <div className='frame30__text'>Looking for something else?</div>
+                    <div className='frame30__button'>
+                        <div className='frame30__button__text'> Start Over</div>
+                    </div>
+                </div>
+            </div>
+
+            <div className='email'>
+                <div className='email__bg'>
+                    <div className="email__frame66">
+                        <div className="frame61">
+                            <div className="frame61__frame60">
+                                <div className="frame61__frame60__text1">Want to save your results for later?</div>
+                                <div className="frame61__frame60__text2">Enter your email to receive your best bag
+                                    match directly to your inbox and be the first to learn about new models releases
+                                    and more.
+                                </div>
+                                <div className="frame61__frame19">
+                                    <div className="frame61__frame19__frame62">
+                                        <div className='frame61__frame19__frame62__title'>Email</div>
+                                        <div className='frame61__frame19__frame62__input' placeholder='Your Email'></div>
+                                    </div>
+
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>

0232719356c6434895a7759aa408c5b487dd9838
Fernando Diaz <diazemanuel323@gmail.com>
2022-10-26 14:28:41 -0600

    px a rem

--- cotopaxi/src/components/Question3.jsx
+++ cotopaxi/src/components/Question3.jsx
@@ -7,53 +7,76 @@ export const Question3 = () => {
         <div>
             <header className="header"></header>
             <div className="bar">
-                <div className="bar__line"></div>
-                <div className="bar__steps">
-                    <div className="bar__steps__frame ">
-                        <div className="step1"></div>
+                    <div className="bar__steps">
+                        <div className="bar__steps__frame ">
+                            <div className="step1 success">
+                                <div className='step1__vector'></div>
+                            </div>
+                        </div>
+                        <div className="bar__steps__number state color">1</div>
+                        <div className='bar__steps__number names__bar color'>Gear</div>
                     </div>
-                    <div className="bar__steps__number">1</div>
-                </div>
-                <div className="bar__steps">
-                    <div className="bar__steps__frame ">
-                        <div className="step1"></div>
+                    <div className="bar__steps">
+                        <div className="bar__steps__frame ">
+                            <div className="step1 success">
+                                <div className='step1__vector'></div>
+                            </div>
+                        </div>
+                        <div className="bar__steps__number state color">2</div>
+                        <div className='bar__steps__number names__bar color'>length</div>
                     </div>
-                    <div className="bar__steps__number">2</div>
-                </div>
-                <div className="bar__steps">
-                    <div className="bar__steps__frame ">
-                        <div className="step1"></div>
+                    <div className="bar__steps">
+                        <div className="bar__steps__frame ">
+                            <div className="step1 step__focus">
+                               
+                            </div>
+                        </div>
+                        <div className="bar__steps__number state color">3</div>
+                        <div className='bar__steps__number names__bar color'>commute</div>
                     </div>
-                    <div className="bar__steps__number">3</div>
-                </div>
-                <div className="bar__steps">
-                    <div className="bar__steps__frame ">
-                        <div className="step1"></div>
+                    <div className="bar__steps">
+                        <div className="bar__steps__frame ">
+                            <div className="step1 ">
+                                
+                            </div>
+                        </div>
+                        <div className="bar__steps__number state">4</div>
+                        <div className='bar__steps__number names__bar'>climate</div>
                     </div>
-                    <div className="bar__steps__number">4</div>
-                </div>
-                <div className="bar__steps">
-                    <div className="bar__steps__frame ">
-                        <div className="step1"></div>
+                    <div className="bar__steps">
+                        <div className="bar__steps__frame ">
+                            <div className="step1">
+                               
+                            </div>
+                        </div>
+                        <div className="bar__steps__number state">5</div>
+                        <div className='bar__steps__number names__bar'>use</div>
                     </div>
-                    <div className="bar__steps__number">5</div>
-                </div>
-                <div className="bar__steps">
-                    <div className="bar__steps__frame ">
-                        <div className="step1"></div>
+                    <div className="bar__steps">
+                        <div className="bar__steps__frame ">
+                            <div className="step1">
+                                
+                            </div>
+                        </div>
+                        <div className="bar__steps__number state">6</div>
+                        <div className='bar__steps__number names__bar'>shoes</div>
                     </div>
-                    <div className="bar__steps__number">6</div>
-                </div>
-                <div className="bar__steps">
-                    <div className="bar__steps__frame ">
-                        <div className="step1"></div>
+                    <div className="bar__steps">
+                        <div className="bar__steps__frame ">
+                            <div className="step1">
+                                
+                            </div>
+                        </div>
+                        <div className="bar__steps__number state">7</div>
+                        <div className='bar__steps__number names__bar'>one-of-a-kind</div>
                     </div>
-                    <div className="bar__steps__number">7</div>
+                    <div className="bar__line"></div>
                 </div>
-            </div>
             <div className="frame10">
                 <div className="frame1">
-                    <div className="frame1__title">How long is your trip?</div>
+                    <div className="frame1__title tittle__mobile">How long is your trip?</div>
+                    <div className="frame1__title tittle__tablet">What do you need a new bag for?</div>
+                    <div className="frame1__title tittle__desk">How will you be traveling with your bag?</div>
                     <div className="frame1__text"> <span className="frame1__text__span">Did you know? </span> While it depends on how light you pack, we recommend
                         around 25-35L for weekend trips and 35-50L for a week or more.
                     </div>
--- cotopaxi/src/index.js
+++ cotopaxi/src/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Question3 } from './components/Question3';
-import { Results } from './components/Results';
-
-// import './css/Question3.css'
-import './css/Results.css'
+// import { Results } from './components/Results';
+// import './css/Results.css'
+import './css/Question3.css'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Results />
+    <Question3 />
   </React.StrictMode>
 );
 
